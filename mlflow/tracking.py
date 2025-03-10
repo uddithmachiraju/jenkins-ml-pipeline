0e75edc78f80d7a698a22c481e9acde4ae602c84
uddithmachiraju <uddithmachiraju@gmail.com>
2025-03-10 11:55:04 +0530

    Model Tracking

--- mlflow/tracking.py
+++ mlflow/tracking.py
@@ -14,7 +14,6 @@ def setup_mlflow():
     """Configures MLFlow tracking server"""
     tracking_uri = "http://127.0.0.1:5000"
     mlflow.set_tracking_uri(tracking_uri)
-    mlflow.set_experiment("Credit Card Fraud Detection") 
     logging.info(f"MLFlow Tracking server started at {tracking_uri}")  
 
 if __name__ == "__main__":
